fd5ebb36fbef1780d0965b97cf3603db624b8770
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-06-06 21:26:26 +0200

    Ajout axios, lancement de la requete get pour récupérer la liste des films populaires

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, {Component} from 'react'
 import SearchBar from '../components/search-bar'
 import VideoList from './video-list'
+import axios from 'axios'
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar/>
-      <VideoList/>
-    </div>
-  )
+const API_END_POINT = 'https://api.themoviedb.org/3/'
+const POPULAR_MOVIES_URL = 'discover/movie?language=fr&sort_by=popularity.desc&include_adult=false&append_to_response=images'
+const API_KEY = 'api_key=25574e6cf05efdc183382e06af719cfc'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  UNSAFE_componentWillMount() {
+    axios.get(`${API_END_POINT}${POPULAR_MOVIES_URL}&${API_KEY}`)
+      .then( function(response) {
+        console.log(response)
+      })  
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar/>
+        <VideoList/>
+      </div>
+    ) 
+  }
 }
 
 export default App
