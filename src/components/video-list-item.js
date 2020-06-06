1a6ac61efd90f04f31d90fd5cac05f41c9092a36
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-06-06 22:59:28 +0200

    Affichage de l'image du film dans chaque video List Item

--- src/components/video-list-item.js
+++ src/components/video-list-item.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500/'
+
 const VideoListItem = props => {
   const {movie} = props
   console.log(movie)
-  return <li>{ movie.title }</li>
+  return (
+    <li>
+      <img height='100px' width='100px' src={`${IMAGE_BASE_URL}${movie.poster_path}`}/>
+      <h5>{ movie.title }</h5>
+    </li>
+  ) 
 }
 
 export default VideoListItem
