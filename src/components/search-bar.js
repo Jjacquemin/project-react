637c54eff407e687793335cc4a0994d417e3f5e6
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-06-06 19:12:15 +0200

    Ajout du state de l'objet search bar, affichage placeholder et event onChange

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,8 +1,15 @@
 import React, {Component} from 'react'
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { searchText:'', placeHolder:'Tapez votre film...' }
+  }
   render() {
-    return <input/>
+    return <input onChange = { this.handleChange } placeholder = { this.state.placeHolder }/>
+  }
+  handleChange(){
+    console.log('Une saisie')
   }
 }
 
